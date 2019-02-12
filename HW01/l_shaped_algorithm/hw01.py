95753babedf093ea788281a54b457eaefee06a78
Jonathan Schuba <35272616+jschuba@users.noreply.github.com>
2019-02-11 22:05:54 -0600

    first working implementation with correct solution

--- HW01/l_shaped_algorithm/hw01.py
+++ HW01/l_shaped_algorithm/hw01.py
@@ -5,14 +5,15 @@ from l_shaped_algorithm_pulp import L_Shaped_Algorithm
 
 c = np.array([3,2])
 
+
 W = np.array([[3,2,1,0,0,0,0,0], 
               [2,5,0,1,0,0,0,0],
-              [1,0,0,0,1,0,0,0],
-              [-1,0,0,0,0,1,0,0],
-              [0,1,0,0,0,0,1,0],
-              [0,-1,0,0,0,0,0,1]])
+              [-1,0,0,0,1,0,0,0],
+              [0,-1,0,0,0,1,0,0],
+              [1,0,0,0,0,0,1,0],
+              [0,1,0,0,0,0,0,1]])
 
-
+'''
 W = np.array([[3,2], 
               [2,5],
               [-1,0],
@@ -20,11 +21,11 @@ W = np.array([[3,2],
               [1,0],
               [0,1]
               ])
-
-
+'''
 h = []
 T = []
 p = []
+q = []
 for s1 in [6,4]:
     for s2 in [8,4]:
         h.append(np.array([0, 0, -0.8*s1, -0.8*s2, s1, s2]))
@@ -35,13 +36,15 @@ for s1 in [6,4]:
                            [0,0],
                            [0,0]]))
         p.append(1/4)
+        q.append(np.array([s1,s2]))
         
         
 
 
 
 
-Solver = L_Shaped_Algorithm(c, None, None, W, h, T, p)
+Solver = L_Shaped_Algorithm(c, None, None, W, h, T, q, p,
+                            max_iter = 10, verbose=False, debug=False)
 
-Solver.solve()
+x_opt = Solver.solve()
 
--- HW01/l_shaped_algorithm/l_shaped_algorithm_pulp.py
+++ HW01/l_shaped_algorithm/l_shaped_algorithm_pulp.py
@@ -18,8 +18,6 @@ import numpy as np
 import pulp as lp
 
 
-from sklearn import preprocessing
-
 
 
 class L_Shaped_Algorithm():
@@ -27,7 +25,8 @@ class L_Shaped_Algorithm():
     
     """
     
-    def __init__(self, c, A, b, W, h, T, p, solver='CVX'):
+    def __init__(self, c, A, b, W, h, T, q, p, solver='CVX', max_iter = 100,
+                 verbose=False, debug=False):
         
         self.c = c
         self.A = A
@@ -35,9 +34,11 @@ class L_Shaped_Algorithm():
         self.W = W
         self.h = h
         self.T = T
+        self.q = q
         self.p = p
         
         self.solver = solver
+        self.max_iter = max_iter
         
         # Check that lengths match
         self.K = len(h)
@@ -68,9 +69,10 @@ class L_Shaped_Algorithm():
         '''
         self.x_nu = []
         self.theta_nu = []
+        self.objective_value = []
         
-        self.debug = False
-        self.verbose = True
+        self.debug = debug
+        self.verbose = verbose
         
         
 
@@ -79,9 +81,12 @@ class L_Shaped_Algorithm():
         
     def solve(self):
         
-        for _ in range(5):
+        for _ in range(self.max_iter):
             self.nu += 1 # iterate step counter
-            print (f"===== Iteration {self.nu} =====")
+            print ()
+            print ( "===========================================")
+            print (f"=============== Iteration {self.nu} ===============")
+            print ( "===========================================")
             x_i, theta_i = self.step_1()
 
             
@@ -89,8 +94,21 @@ class L_Shaped_Algorithm():
             if np.any(D_step2) != None:
                 # A feasibility cut was made
                 continue
-            else :
-                print ("No step 2 cuts made")
+            #else 
+            print ("No feasibility cuts made")
+            E_step3, e_step3 = self.step_3()
+            if np.any(E_step3) == None:
+                # optimal solution found
+                print ("Optimal Solution Found")
+                print ()
+                print ("Objective Value  = ", self.objective_value[-1])
+                print ("Optimal Solution = ", self.x_nu[-1])
+                self.value = self.objective_value[-1]
+                self.solution = self.x_nu[-1]
+                
+                break
+            
+        return self.x_nu[-1]
         
         
     def step_1(self):
@@ -98,8 +116,8 @@ class L_Shaped_Algorithm():
         Solve the linear program with any constraints imposed by previous
         feasibility and optimality cuts
         '''
-        print ()        
-        print (f"Starting Step 1 for iteration {self.nu}")
+      
+        print (f"     ------ Iteration {self.nu}, Step 1 ------")
         
         n = len(self.c)         
         x = cp.Variable(n)
@@ -115,10 +133,9 @@ class L_Shaped_Algorithm():
         if self.A is not None:
             constraints.append( self.A @ x == self.b )
                        
-        
+            
         for i in range(len(self.D)):
             # add constraints for each feasibility cut
-            print ("Adding feasibility cut")
             constraints.append( self.D[i] @ x >= self.d[i])
         for i in range(len(self.E)):
             # add constraints for each optimality cut
@@ -129,6 +146,7 @@ class L_Shaped_Algorithm():
         
         x_solution = np.array(x.value).reshape([x.value.size])
         
+        self.objective_value.append(result)
         print ("x_nu = ")
         print (x_solution)
         print ("theta_nu = ")
@@ -158,11 +176,11 @@ class L_Shaped_Algorithm():
         m = self.W.shape[1]
         
         print ()
-        print (f"Starting Step 2 for iteration {self.nu}")
+        print (f"     ------ Iteration {self.nu}, Step 2 ------")
         
         for k in range(self.K):
             lp_name = f"step2_iteration{self.nu}_k={k}"
-            print (lp_name)
+            
             
             prob = lp.LpProblem(lp_name, lp.LpMinimize)
             
@@ -188,8 +206,7 @@ class L_Shaped_Algorithm():
 
             prob.solve()
         
-            print("Status:", lp.LpStatus[prob.status])
-            print("objective=", lp.value(prob.objective))
+            
             
             if self.debug:
                 for v in prob.variables():
@@ -202,28 +219,27 @@ class L_Shaped_Algorithm():
             
             if lp.value(prob.objective) > 0:
                 # Then we need to add a feasibility cut
+                self.r += 1
                 
-                print ("Feasibility cut needed")
                 # Get the dual variables
                 sigma = []    
                 for name, c in prob.constraints.items():
                     sigma.append(c.pi)
-                
                 sigma = np.array(sigma)
+                
+                print ("Feasibility cut identified")
+                print (lp_name)
+                print ("objective = ", lp.value(prob.objective))
                 print ("dual objective = ", (np.abs(self.h[0]) - self.T[0]@self.x_nu[-1]) @ sigma)
-        
-                print ("dual variables = ")
-                print (sigma)
+                print ("dual variables = ", sigma)
                 
                 D = sigma.T @ self.T[k]
                 d = sigma.T @ np.abs(self.h[k])
                 if d < 0.0:
                     d *= -1
                     D *= -1
-                print ("Dk = ")
-                print (D)
-                print ("dk = ")
-                print (d)
+                print ("Dk = ", D)
+                print ("dk = ", d)
                 self.D.append(D)
                 self.d.append(d)
                 return D, d
@@ -233,4 +249,92 @@ class L_Shaped_Algorithm():
         
         
     def step_3(self):
-        pass
+        '''
+        Solve LPs for each possible realization of the random variable, and 
+        make optimality cuts as appropriate
+        '''
+        n = self.W.shape[0]
+        m = self.W.shape[1]
+        
+        print ()
+        print (f"     ------ Iteration {self.nu}, Step 3 ------")
+        
+        # Setup the variables E and e
+        E = np.zeros(self.T[0].shape[1])
+        e = 0
+        
+        for k in range(self.K):
+            lp_name = f"step3_iteration{self.nu}_k={k}"
+            
+            prob = lp.LpProblem(lp_name, lp.LpMinimize)
+            
+            y = lp.LpVariable.matrix("y", indexs=list(range(m)), lowBound=0)
+            
+            # Define the objective function
+            prob += lp.lpDot(self.q[k], y), "obj"
+
+            for i in range(n):
+                if self.h[0][i] >= 0:
+                    prob += ( lp.lpDot(self.W[i], y) <= 
+                             self.h[k][i] - self.T[k][i] @ self.x_nu[-1], 
+                             "constraint" + str(i) )
+                else:
+                    prob += ( lp.lpDot(-1*self.W[i], y) >= 
+                             -1*self.h[k][i] - self.T[k][i] @ self.x_nu[-1], 
+                             "constraint" + str(i) )
+
+            prob.solve()
+        
+            
+            
+            if self.debug:
+                print (lp_name)
+                for v in prob.variables():
+                    print(v.name, "=", v.varValue, "\tReduced Cost =", v.dj)
+
+                print("\nSensitivity Analysis\nConstraint\t\t\t\t\t\tShadow Price\tSlack")
+                for name, c in prob.constraints.items():
+                    print(name, ":", c, "\t", c.pi, "\t\t\t\t\t\t", c.slack)    
+    
+            
+            # Get the dual variables
+            pi = []    
+            for name, c in prob.constraints.items():
+                pi.append(c.pi)
+            
+            pi = np.array(pi)
+            
+            if self.debug:
+                print("objective=", lp.value(prob.objective))
+                print ("dual objective = ", (np.abs(self.h[0]) - self.T[0]@self.x_nu[-1]) @ pi)
+                print ("dual variables = ", pi)
+
+            for k in range(self.K):
+                E += self.p[k] * pi.T @ self.T[k]
+                e += self.p[k] * pi.T @ np.abs(self.h[k])
+        
+          
+        if e < 0.0:
+            e *= -1
+            E *= -1
+        w_nu = e - E @ self.x_nu[-1]
+        
+        if self.verbose:
+            print ("w_nu = ", w_nu)
+            print ("theta_nu = ", self.theta_nu[-1])
+            
+        if self.theta_nu[-1] >= w_nu and self.theta_nu[-1] is not None:
+            # The solution is optimal
+            return None, None
+        
+        # Else append optimality cut
+        print ("Optimality cut made")
+        print ("E = ", E)
+        print ("e = ", e)
+        self.s += 1
+        self.E.append(E)
+        self.e.append(e)
+        return E, e
+    
+
+        
